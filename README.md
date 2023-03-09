6a1a3d17b826ba3985cbac25343864cd95103eef
unknown <sirnodity@gmail.com>
2023-03-09 11:29:00 +0300

    The first version of the bot has been added

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
+<span style='font-size: 16px;'>
+
+
 # HowLong2Bot
-Chat bot for Twitch, which interacts with the service howlongtobeat.com and provides various functionalities for the streamer and his viewers
+<p style='font-size: 20px;'>A small chat bot for Twitch.tv, which interacts with the service howlongtobeat.com and provides various functionalities for the streamer and his viewers</p>
+
+# About 
+This small chat bot for Twitch, with the help of commands in the chat provides an opportunity to find out the approximate time required to pass the game specified in the category of the current broadcast
+
+# Current commands
+<b style='font-size: 18px;'>!walkthrough</b> - Show the passing time of the game specified in the broadcast category
+
+</span>
---
+++ app.js
@@ -0,0 +1,47 @@
+"use strict";
+import * as dotenv from 'dotenv';
+import axios from "axios";
+import howlongtobeat from "howlongtobeat";
+import tmi from "tmi.js";
+let hltbService = new howlongtobeat.HowLongToBeatService();
+dotenv.config()
+
+
+const instance = axios.create({
+    baseURL: "https://api.twitch.tv/helix",
+    withCredentials: true,
+    'Content-Type': 'application/json',
+    headers: {
+        Authorization: process.env.auth,
+        "Client-Id": process.env.client,
+    }
+})
+
+
+const client = new tmi.Client({
+    options: { debug: true },
+    identity: {
+        username: process.env.botName,
+        password: process.env.access
+    },
+    channels: [process.env.channel1name, process.env.channel2name]
+});
+
+client.connect();
+
+client.on("message", (channel, tags, message, self) => {
+
+    if (self) return;
+
+    switch (message.toLowerCase()) {
+        case "!walkthrough":
+            instance.get(`/channels?broadcaster_id=${process.env.channel2name}`).then(response => {
+                response.data && hltbService.search(response.data.data[0].game_name).then(result => {
+                    client.say(channel, `@${tags.username}, Примерное время прохождения ${result[0].name} - ${result[0].gameplayMain} часа(ов)`);
+                });
+            })
+            break;
+        default:
+            return
+    }
+});
